b4f33f90308928b4991789711bb41a0516c02b4c
pervincaliskan <pervincaliskan01@hotmail.com>
2023-06-10 18:51:56 +0300

    header compenent added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,31 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        screens: {
+          xs: "375px",
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1140px",
+          "2xl": "1140px",
+        },
+      },
+      colors: {
+        primary: "#ffbe33",
+        secondary: "#222831",
+      },
+      fontFamily: {
+        dancing: ["Dancing Script", "cursive"],
+        sans: ["Open Sans", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
