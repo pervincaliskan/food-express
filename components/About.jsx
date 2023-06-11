88c40c87f67e38787c26cddd46fb13ac1c8b4270
pervincaliskan <pervincaliskan01@hotmail.com>
2023-06-11 20:20:05 +0300

    About updated

--- components/About.jsx
+++ components/About.jsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Title from "./ui/Title";
-
 const About = () => {
   return (
     <div className="bg-secondary py-14">
       <div className="container mx-auto flex items-center text-white gap-20 justify-center flex-wrap-reverse">
         <div className="flex justify-center">
-          <div className="relative sm:w-[445px] sm-h-[600px]  flex justify-center w-[300px] h-[450px]">
+          <div className="relative sm:w-[445px] sm:h-[600px]  flex justify-center w-[300px] h-[450px]">
             <Image src="/images/about-img.png" alt="" layout="fill" />
           </div>
         </div>
@@ -26,5 +25,4 @@ const About = () => {
     </div>
   );
 };
-
 export default About;
