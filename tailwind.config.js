c4605298cfdf9989c03b9acfe69f8f88e578f06b
pervincaliskan <pervincaliskan01@hotmail.com>
2023-06-18 02:05:59 +0300

    tailwind.config.js updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,12 +23,14 @@ module.exports = {
       colors: {
         primary: "#ffbe33",
         secondary: "#222831",
+        danger: "#ff0000",
+        success: "#00ff00",
       },
       fontFamily: {
         dancing: ["Dancing Script", "cursive"],
         sans: ["Open Sans", "sans-serif"],
-      }
+      },
     },
   },
   plugins: [],
-}
+};
