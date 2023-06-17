06e7f6435c4874d371182070113884a1ad327a56
pervincaliskan <pervincaliskan01@hotmail.com>
2023-06-17 11:54:35 +0300

    Pages updated

--- pages/home/index.js
+++ pages/home/index.js
@@ -2,15 +2,21 @@ import React from "react";
 import About from "../../components/About";
 import Campaigns from "../../components/Campaigns";
 import Carousel from "../../components/Carousel";
+import Customers from "../../components/customers/Customers";
 import MenuWrapper from "../../components/product/MenuWrapper";
+import Reservation from "../../components/Reservation";
+
 const Index = () => {
   return (
-    <div>
+    <React.Fragment>
       <Carousel />
       <Campaigns />
       <MenuWrapper />
       <About />
-    </div>
+      <Reservation />
+      <Customers />
+    </React.Fragment>
   );
 };
+
 export default Index;
